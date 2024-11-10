402074324a5cec4c2b6418cd5b8f3577892c5036
Kostia06 <ilnkostia@gmail.com>
2024-11-10 01:50:07 -0700

    base

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,10 +1,114 @@
-// Home page
-import Avatar from "../../components/avatars/index.js";
+import React from "react";
+import Avatar from "@/components/avatars/index.js";
+import { colors, user, chats } from "@/lib/utils";
 
 
-export default function Page() {
-  return(
-    <div>
-    </div> 
-  )
+const chatBubble = (messages: any, i: int) => {   
+    const avatarCSS = `
+        w-12 h-12
+        ml-4
+        chat-image avatar
+    `;
+    const messageCSS = `
+        h-full w-full
+    `;
+    const messageLeftCSS = `
+        chat chat-start
+    `;
+    const messageRightCSS = `
+        chat chat-end
+    `;
+
+
+    return(
+       <div key={i} className={user.displayName !== messages.author.displayName ? messageLeftCSS : messageRightCSS}>
+            <Avatar name={messages.author.displayName} colors={colors} variant="beam" className={avatarCSS} />
+            <div className="chat-header flex items-center space-x-3">
+                <h1>{messages.author.displayName}</h1>
+                <h1 className="text-xs opacity-50">{messages.author.pronouns[0]}/{messages.author.pronouns[1]}</h1>
+            </div>
+            <div className="chat-bubble bg-gray-200 text-black">
+                {messages.content}
+            </div>
+        </div>
+    )    
+};
+
+
+
+
+export default function Chat(chatId: string){
+
+
+    let obj;
+    for (let i = 0; i < chats.length; i++){
+        if(chats[i].chatId === chatId.chatId){
+            obj = chats[i];
+            break;
+        }
+    }
+
+    if(!obj){
+        return <h1>Chat id doesnt exist</h1>
+    }
+
+
+
+    const chatCSS = `
+        relative
+        h-full w-full
+        flex flex-col
+    `;
+    const headerCSS = `
+        h-20 w-full
+        bg-gray-100
+        flex items-center
+    `;
+    
+    const titleCSS = `
+        w-full h-20
+        flex items-center justify-center
+        text-2xl font-bold
+    `;
+
+    const avatarCSS = `
+        w-14 h-14
+        ml-4
+        ease duration-700
+        hover:rotate-[360deg]
+    `;
+
+    const messagesCSS = `
+        h-full w-full
+        flex flex-col
+    `;
+
+    const inputAreaCSS = `
+        input input-border 
+        w-1/2 max-w-xs
+        bg-gray-200
+        text-gray-800
+    `;
+
+    const inputCSS = `
+        h-20 w-full
+        flex items-center justify-center
+    `;
+
+    return(
+        <div className={chatCSS}>
+            <div className={headerCSS}>
+                <Avatar name={obj.chatId} colors={colors} variant="bauhaus" className={avatarCSS} />
+                <div className={titleCSS}>
+                    <h1>{obj.chatName}</h1>
+                </div>
+            </div>
+            <div className={messagesCSS}>
+                {obj.messages.map((message: any, i: int) => chatBubble(message, i))}
+            </div>
+            <div className={inputCSS}>
+                <input type="text" placeholder="Type here" className={inputAreaCSS}/>
+            </div>
+        </div>
+    )
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,35 +1,185 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Avatar from "../avatars/index.js";
+import { colors, pronouns, user } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
-export default function NavBar(){
 
-    const links = [
-        {name: "Home", href: "/", icon: "home"},
-        {name: "About", href: "/about", icon: "info"},
-        {name: "Chats", href: "/chats", icon: "chat"},
-        {name: "Profile", href: "/profile", icon: "person"},
-    ];    
-    
-    const colors = ["#0a0310", "#49007e", "#ff005b", "#ff7d10", "#ffb238"];
-    const name = "Kostia";
 
-    const linkCSS = "flex items-center justify-center h-14 w-14";
+const account = (user: any) => {
+    const avatarCSS = `
+        h-14 w-14
+        outline-none
+        ease duration-700
+        hover:rotate-[360deg] 
+    `;
 
     return(
-        <ul className="absolute h-full w-20 bg-gray-100 flex flex-col h-full items-center py-5">
-            <li className={linkCSS}>
-                <Avatar name={name} colors={colors} variant="beam" size={50}/>
-            </li>
+        <Dialog>
+            <DialogTrigger>
+                <Avatar name={user.displayName} colors={colors} variant="beam" className={avatarCSS} />
+            </DialogTrigger>
+            <DialogContent>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                    Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input id="username" defaultValue="@Pablo" className="col-span-3"/>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Name
+                        </Label>
+                        <Input id="name" defaultValue="Pablo" className="col-span-3"/>
+                    </div>  
+                    <div className="grid grid-cols-4 gap-4 flex items-center">
+                        <Label htmlFor="pronouns" className="text-right">
+                            Pronouns
+                        </Label>
+                        <div className="join">
+                            <Select>
+                                <SelectTrigger className="w-min join-item">
+                                    <SelectValue placeholder={user.pronouns[0]} />
+                                </SelectTrigger>
+                                <SelectContent side="down" className="max-h-64">
+                                    {pronouns.map((pronoun, i) => (
+                                        <SelectItem key={i} value={pronoun}>
+                                            {pronoun}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <Select>
+                                <SelectTrigger className="w-min join-item">
+                                    <SelectValue placeholder={user.pronouns[1]} />
+                                </SelectTrigger>
+                                <SelectContent side="down" className="max-h-64">
+                                    {pronouns.map((pronoun, i) => (
+                                        <SelectItem key={i} value={pronoun}>
+                                            {pronoun}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div> 
+                    </div>
+                </div>            
+                <DialogFooter>
+                    <Button type="submit">
+                        Save
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
 
-            <div className="divider bg-red-200"></div> 
-            {links.map((link, index) => (
-                <li className={linkCSS} key={index}>
-                    <Link  href={link.href} className="flex items-center justify-center h-full w-full">
-                        <i className="material-icons">{link.icon}</i>
-                    </Link>
-                </li>
-            ))}
-        </ul>
+
+const signUp = () => {
+    return(
+        <Dialog>
+            <DialogTrigger>
+                ?
+            </DialogTrigger>
+            <DialogContent>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                    Make changes to your profile here. Click save when you're done.
+                </DialogDescription>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="username" className="text-right">
+                            Username
+                        </Label>
+                        <Input id="username" defaultValue="@Pablo" className="col-span-3"/>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Name
+                        </Label>
+                        <Input id="name" defaultValue="Pablo" className="col-span-3"/>
+                    </div>  
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="pronouns" className="text-right">
+                            Pronouns
+                        </Label>
+                        <Input id="pronouns" defaultValue="He/She" className="col-span-3"/>
+                    </div>
+                </div>            
+                <DialogFooter>
+                    <Button type="submit">
+                        Save
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+
+    )
+}
+
+
+
+export default function NavBar(){
+    const links = [
+        {name: "Home", href: "/", icon: "home"},
+        {name: "Chats", href: "/chats", icon: "chat"},
+        {name: "Search", href: "/search", icon: "search"},
+    ]
+
+    const itemCSS = `
+        flex items-center justify-center
+        h-14 w-14 
+        ease duration-500
+        bg-gray-200 hover:bg-gray-300
+        rounded-full
+    `;
+    const linkCSS = `
+        flex items-center justify-center
+        h-full w-full
+    `;
+    const navCSS = `
+        h-full w-20
+        bg-gray-100
+        py-5  
+        flex flex-col items-center
+        space-y-5
+    `;
+    const separator1CSS = `
+        m-2
+        h-1 w-2/3
+        rounded-full
+    `;
+
+    const separator2CSS = `
+        h-full w-1
+    `;
+
+    return(
+        <>
+            <ul className={navCSS}>
+                {user && account(user)}
+                <Separator className={separator1CSS}/>
+                {links.map((link) => (
+                    <li className={itemCSS} key={link.icon}>
+                        <Link  href={link.href} className={linkCSS}>
+                            <i className="material-icons">{link.icon}</i>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <Separator orientation="vertical" className={separator2CSS}/>
+        </>
+
     )
 }
